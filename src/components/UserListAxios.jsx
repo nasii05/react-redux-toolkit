b87807ae8c86aa1bebdaf0a70634090e79f35ddb
nasii05 <iamnasih@gmail.com>
2025-07-24 19:23:43 +0530

    fix

--- src/components/UserListAxios.jsx
+++ src/components/UserListAxios.jsx
@@ -2,35 +2,41 @@ import axios from 'axios';
 import { useEffect, useState } from 'react'
 
 const UserListAxios = () => {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  
-  useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/users')
-       .then(response => {
-            setUsers(response.data);
-            setIsLoading(false);
-       })
-       .catch(error => {
-            console.error("There was an error fetching the users!", error);
-            setIsLoading(false);
-       });
-  },[])
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-  if(isLoading) return <div>Loading...</div>;
+    const getData =  async () => {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                setUsers(response.data);
+                setIsLoading(false);
+            })
+            .catch(error => {
+                console.error("There was an error fetching the users!", error);
+                setIsLoading(false);
+            });
+    }
 
-  return (
-    <div>
-      <h2>User List</h2>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>
-            {user.name} - {user.email}
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+    useEffect(() => {
+        setTimeout(() => {
+          getData();
+        }, 2000);
+    }, [])
+
+    if (isLoading) return <div>Loading...</div>;
+
+    return (
+        <div>
+            <h2>User List</h2>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>
+                        {user.name} - {user.email}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default UserListAxios
